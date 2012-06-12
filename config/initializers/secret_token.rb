d0afe01de0a1af4c88792943908ccaf9a1b8f4a0
Jeffrey L Sullivan Jr <jeff.sullivan.jr@me.com>
2012-06-11 22:26:09 -0700

    Initial commit including all Gems from listing 9.49 in the Ruby on Rails Tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6afaa9127771935994f63bba47bfec362e36f1496a6babc45b5010c33a1f863e59ac86ffddea69a4d0c49cecaf3f6261e960c1956df5d3f8d16bad1356e219c5'
