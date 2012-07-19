fbf46500270c74e6b49375da9932790acdfde935
Jeffrey L Sullivan Jr <jeff.sullivan.jr@me.com>
2012-07-18 18:41:31 -0700

    Routing and css

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,19 +6,19 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App |" }
   describe "Home page" do
   
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')    
     end
     
     it "should have the base title" do 
-  		visit '/static_pages/home'
+  		visit root_path
   		page.should have_selector('title', 
   									:text => 
   									"Ruby on Rails Tutorial Sample App")
   	end
   	
   	it "should not have a custom page title" do
-  		visit '/static_pages/home'
+  		visit root_path
   		page.should_not have_selector('title', :text => '| Home')
   	end
   end
@@ -27,18 +27,18 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App |" }
   describe "Help page" do
   	
   	it "should have the h1 'Help'" do 
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_selector('h1', :text => 'Help')
   	end
   	
   	it "should have title 'Help'" do 
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_selector('title', :text => 
   		"Ruby on Rails Tutorial Sample App")
   	end
   	
   	it "should not have a custom page title" do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should_not have_selector('title', :text => '| Help')
   	end
   end
@@ -46,19 +46,19 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App |" }
   describe "About page" do
   	
   	it "should have the content 'About Us'" do 
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('h1', :text => 'About Us')
   	end
   	
   	 it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
                     :text => 
   									"Ruby on Rails Tutorial Sample App")
   end
   
   it "should not have a custom page title" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should_not have_selector('title', :text => '| About')
   	end
   end
@@ -66,18 +66,18 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App |" }
   describe "Contact page" do
   
   it "should have the content 'Contact'" do
-  	visit '/static_pages/contact'
+  	visit contact_path
   	page.should have_selector('h1', :text => 'Contact')
   	end
   	
   it "should have the title 'Contact'" do
-  	visit '/static_pages/contact'
+  	visit contact_path
   	page.should have_selector('title', 
   								:text => 
   									"Ruby on Rails Tutorial Sample App")
   	end
   it "should not have a custom page title" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		page.should_not have_selector('title', :text => '| Contact')
   	end	
   end
