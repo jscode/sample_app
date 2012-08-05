89221c55272c4fbb260f6068c781413470431dcf
Jeffrey L Sullivan Jr <jeff.sullivan.jr@me.com>
2012-08-04 18:05:52 -0700

    Chapter 5 Exercises, full_title helper removed, tests updated

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,12 @@
 # Returns the full title on a per-page basis.
- def full_title(page_title)
-	 base_title = "Ruby on Rails Tutorial Sample App"
-	 if page_title.empty?
-		 base_title
-	 else
-		 "#{base_title} | #{page_title}"
-	 end
- end
+
+# include ApplicationHelper
+# 
+#  def full_title(page_title)
+# 	 base_title = "Ruby on Rails Tutorial Sample App"
+# 	 if page_title.empty?
+# 		 base_title
+# 	 else
+# 		 "#{base_title} | #{page_title}"
+# 	 end
+#  end
