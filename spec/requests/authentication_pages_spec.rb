5d1f8314b87d88704ea5377d99de0db1139450d1
Jeffrey L Sullivan Jr <jeff.sullivan.jr@me.com>
2013-02-07 20:59:31 -0800

    Finish sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,17 +11,22 @@ describe "Authentication" do
   		before { click_button "Sign in" }
 
   		it { should have_selector('title', text: 'Sign in') }
-  		it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+  		#it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_error_message ('Invalid') }
 		end
   	#it { should have_selector('h1',    text: 'Sign in') }
     #it { should have_selector('title', text: 'Sign in') }
- 		describe "with valid information" do
- 			let(:user) { FactoryGirl.create(:user) }
- 			before do
- 				fill_in "Email", with: user.email
- 				fill_in "Password", with: user.password 
- 				click_button "Sign in"
- 			end
+ 		# describe "with valid information" do
+ 		# 	let(:user) { FactoryGirl.create(:user) }
+ 		# 	before do
+ 		# 		fill_in "Email", with: user.email
+ 		# 		fill_in "Password", with: user.password 
+ 		# 		click_button "Sign in"
+ 		# 	end
+      describe "with valid information" do
+        let(:user) { FactoryGirl.create(:user) }
+        before { valid_signin(user) }
+        
 
  			it { should have_selector('title', text: user.name) }
  			it { should have_link('Profile', href: user_path(user)) }
@@ -36,7 +41,8 @@ describe "Authentication" do
  				before { click_button "Sign in" }
 
  				it { should have_selector('title', text: 'Sign in') }
- 				it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+ 				#it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+        it { should have_error_message('Invalid') }
 
  				describe "after visiting another page" do
  					before { click_link "Home" }
